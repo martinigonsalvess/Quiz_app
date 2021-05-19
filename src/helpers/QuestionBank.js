c345610040dee7b5d2217c1bbc4420cddb7bf6de
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-19 21:46:16 +0200

    created questions in questionBank

---
+++ src/helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "What is 1 + 3?",
+    optionA: "24",
+    optionB: "21",
+    optionC: "4",
+    optionD: "2",
+    answer: "optionC",
+  },
+  {
+    prompt: "Which is a streaming Platform?",
+    optionA: "Twitter",
+    optionB: "Twitch",
+    optionC: "Google",
+    optionD: "Robinhood",
+    answer: "optionB",
+  },
+  {
+    prompt: "Best tech youtube channel?",
+    optionA: "PedroTech",
+    optionB: "Dev Ed",
+    optionC: "Fireship",
+    optionD: "TechWithTim",
+    answer: "optionA",
+  },
+];
