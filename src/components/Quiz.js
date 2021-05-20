f4fc157904dcdf7debb505841107faf278dca5d6
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-20 10:04:33 +0200

    component quiz, add options button

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Questions } from "../helpers/QuestionBank";
 
 function Quiz() {
-  return <div>Quiz</div>;
+  const [currQuestion, setCurrQuestion] = useState(0);
+  return (
+    <div className="Quiz">
+      <h1>{Questions[currQuestion].prompt}</h1>
+      <div className="options">
+        <button>{Questions[currQuestion].optionA}</button>
+        <button>{Questions[currQuestion].optionB}</button>
+        <button>{Questions[currQuestion].optionC}</button>
+        <button>{Questions[currQuestion].optionD}</button>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
